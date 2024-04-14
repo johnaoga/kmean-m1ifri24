ad2f985b6dcc9496f5d6c481c96cabab08015aa6
ludndev <ludndev@gmail.com>
2024-04-14 17:00:13 +0100

    feat: add processing

--- pages/judicaelahyi.py
+++ pages/judicaelahyi.py
@@ -1,6 +1,7 @@
 """
 
 """
+import numpy as np
 import streamlit as st
 import pandas as pd
 
@@ -9,6 +10,22 @@ def load_data(file_path):
     """
     """
     return pd.read_csv(file_path)
+
+
+def k_means(data, k, max_iterations=100):
+    """
+    """
+    centroids = data.sample(n=k, replace=True).values
+    for _ in range(max_iterations):
+        clusters = np.argmin(np.linalg.norm(data.values[:, None] - centroids, axis=2), axis=1)
+        new_centroids = np.array([data[clusters == i].mean(axis=0) for i in range(k)])
+        if np.array_equal(centroids, new_centroids):
+            break
+        centroids = new_centroids
+    return {
+        'clusters': clusters,
+        'centroids': centroids,
+    }
 
 
 def main():
@@ -37,12 +54,19 @@ def main():
         max_value=len(data),
     )
 
-    i = st.number_input(
+    max_i = st.number_input(
         "Enter max number of iterations",
         step=1,
         value=100,
         min_value=1,
     )
+
+    if st.button("Process Clustering"):
+        k_result = k_means(data, k, max_i)
+        clusters = k_result['clusters']
+        centroids = k_result['centroids']
+        st.success("Processing is completed")
+        st.write(clusters, centroids)
 
 
 if __name__ == "__main__":
