ce901d2099ac5c90c754bf7ff55bd40b3ed0c560
ludndev <ludndev@gmail.com>
2024-04-14 18:10:02 +0100

    feat: add intra and inter-cluster metrics

--- pages/judicaelahyi.py
+++ pages/judicaelahyi.py
@@ -30,6 +30,30 @@ def k_means(data, k, max_iterations=100):
     }
 
 
+def compute(type_, data, centroids, clusters):
+    _ = 0
+    centroid_global = np.mean(data, axis=0) # useful only for between case
+    for i, centroid in enumerate(centroids):
+        if type_ == 'with':
+            _ += np.sum((data[clusters == i] - centroid) ** 2)
+        elif type_ == 'between':
+            _ += len(data[clusters == i]) * np.sum((centroid - centroid_global) ** 2)
+    return _
+
+
+def display_appreciation_based_on_metric(metric_value):
+    if metric_value < 100:
+        return ":green[Excellent]"
+    elif metric_value < 500:
+        return ":blue[Fine]"
+    elif metric_value < 1000:
+        return ":gray[Good]"
+    elif metric_value < 2000:
+        return ":orange[Average]"
+    else:
+        return ":red[Poor]"
+
+
 def main():
     st.title("K-Means Clustering - Judicael AHYI")
     st.subheader("IFRI 2024, Master Génie Logiciel, Semestre 2")
@@ -107,6 +131,21 @@ def main():
                 distances = np.linalg.norm(centroids - point_values, axis=1)
                 cluster_prediction = np.argmin(distances)
                 st.write(f"The point is predicted to belong to cluster {cluster_prediction + 1}.")
+
+        st.header("Intra and Inter-cluster Metrics")
+        clusters = st.session_state.get('clusters')
+        if centroids is not None and clusters is not None:
+            w_ = compute('with', data, centroids, clusters)
+            b_ = compute('between', data, centroids, clusters)
+            st.write(f"Sum of squares")
+            st.write(f"-- within clusters: {w_.iloc[0]} - {display_appreciation_based_on_metric(w_.iloc[0])}")
+            st.write(f"-- between clusters: {b_} - {display_appreciation_based_on_metric(b_)}")
+            for i, centroid in enumerate(centroids):
+                cluster_data = data[clusters == i]
+                st.subheader(f"Cluster {i}")
+                st.write("Number of elements:", len(cluster_data))
+                st.write("Standard deviation:", np.std(cluster_data, axis=0))
+                st.write("Mean:", np.mean(cluster_data, axis=0))
 
 
 if __name__ == "__main__":
