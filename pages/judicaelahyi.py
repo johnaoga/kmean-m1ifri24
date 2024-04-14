0571b97c39f947a7b11c092a6366f9182c48d2f7
ludndev <ludndev@gmail.com>
2024-04-14 16:29:57 +0100

    feat: add judicaelahyi.py
    add title and sub header

---
+++ pages/judicaelahyi.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+import streamlit as st
+
+
+def main():
+    st.title("K-Means Clustering - Judicael AHYI")
+    st.subheader("IFRI 2024, Master Génie Logiciel, Semestre 2")
+
+
+if __name__ == "__main__":
+    main()
